ce17beeb79e345fbb6cf383d529fe627ead0cf92
sneha039 <123440006+sneha039@users.noreply.github.com>
2023-02-24 18:30:18 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,19 @@
+# a,b=6,5
+# print('The value of a+b is',a+b)
+# print('The value of a+b is'+''+str(a+b))
+# print('The value of a+b is'+' '+str(a+b))
+# print('The value of a+b is'+'  '+str(a+b))
+
+# a,b,c,d=5,6,7,8
+# print(a,b,c,d)
+# print(str(a)+'  '+str(b)+'  '+str(c)+'  '+str(d))
+# print(a,b,c,d,sep='')
+# print(a,b,c,d,sep=' ')
+# print(a,b,c,d,sep='  ')
+# print(a,b,c,d,sep='->')
+
+# print('hi'  'hello')
+# print('hi' , 'hello')
+# print('hi'+'  '+'hello')
+# print('hi'  'hello',sep='-')
+# print('hi' , 'hello',sep='-')
